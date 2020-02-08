ade5fb1df36280aa17678a59acce3a6c70532ead
Shumitu <darkdragon5255@gmail.com>
2020-02-08 01:44:03 +0100

    add: test for change of user email

--- src/test/java/tests/ThirdTest.java
+++ src/test/java/tests/ThirdTest.java
@@ -1,27 +1,79 @@
 package tests;
 
+import driver.DriverManager;
 import driver.DriverUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import page.objects.LoginPage;
+import waits.WaitForElement;
 
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.*;
 
 public class ThirdTest extends TestBase {
 
     @Test
-    public void locationTest() throws InterruptedException {
+    public void changeUserEmailTest() throws InterruptedException {
+        //login page
         DriverUtils.navigateToPage("https://35.234.114.2:8181/faces/common/signIn.xhtml");
 
+        //login
         LoginPage loginPage = new LoginPage();
         boolean isLoggedIn = loginPage
-                .typeIntoUserNameField("JDoe")
-                .typeIntoPasswordField("P@ssw0rd")
+                .typeIntoUserNameField("DMitchell")
+                    .typeIntoPasswordField("P@ssw0rd")
                 .clickOnLoginButton()
-                .isBannerAfterLoginUsername("JDoe");
+                .isBannerAfterLoginUsername("DMitchell");
         assertTrue(isLoggedIn);
-        Thread.sleep(10_000);
-        assertTrue(true);
 
+        //click dropdown
+        WebElement dropdownAccounts = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/nav[1]/div[1]/div[2]/ul[1]/li[4]/a[1]"));
+        WaitForElement.waitUntilElementIsVisible(dropdownAccounts);
+        dropdownAccounts.click();
 
+        //click list accounts
+        WebElement accountButton = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/nav[1]/div[1]/div[2]/ul[1]/li[4]/ul[1]/li[2]/a[1]"));
+        accountButton.click();
+
+        //get current email of third account
+        String currentEmail = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/div[1]/form[1]/table[1]/tbody[1]/tr[3]/td[4]")).getText();
+
+        //click edit button for third account
+        WebElement accountEditButton = DriverManager.getWebDriver().findElement(By.xpath( "/html[1]/body[1]/div[1]/div[3]/div[1]/form[1]/table[1]/tbody[1]/tr[3]/td[5]/input[2]"));
+        accountEditButton.click();
+
+        //insert new email
+        WebElement emailField = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/div[1]/form[1]/table[1]/tbody[1]/tr[4]/td[2]/input[1]"));
+        WaitForElement.waitUntilElementIsVisible(emailField);
+        emailField.sendKeys("testowy@email.com");
+
+        //click save button
+        WebElement createButton = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/div[1]/form[1]/input[2]"));
+        WaitForElement.waitUntilElementIsVisible(createButton);
+        createButton.click();
+
+        //click dropdown once again
+        WaitForElement.waitUntilElementIsVisible(dropdownAccounts);
+        dropdownAccounts.click();
+
+        //click list accounts once again
+        accountButton.click();
+
+        //get current email of third account
+        String currentEmailAfterChanges = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/div[1]/form[1]/table[1]/tbody[1]/tr[3]/td[4]")).getText();
+
+        assertNotEquals(currentEmailAfterChanges, currentEmail);
+        assertEquals(currentEmailAfterChanges, "testowy@email.com");
+
+//        //click edit button for third account once again
+//        accountEditButton.click();
+//
+//        //insert new email
+//        WaitForElement.waitUntilElementIsVisible(emailField);
+//        emailField.sendKeys(currentEmail);
+//
+//        //click save button
+//        WaitForElement.waitUntilElementIsVisible(createButton);
+//        createButton.click();
     }
 }
