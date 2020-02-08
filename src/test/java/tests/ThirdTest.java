67c61a933e916c02a461cdae94817d2cd477e97e
Shumitu <darkdragon5255@gmail.com>
2020-02-08 13:36:19 +0100

    next test

--- src/test/java/tests/ThirdTest.java
+++ src/test/java/tests/ThirdTest.java
@@ -39,50 +39,47 @@ public class ThirdTest extends TestBase {
 
         //get current email of third account
         String currentEmail = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/div[1]/form[1]/table[1]/tbody[1]/tr[3]/td[4]")).getText();
-        System.out.println(currentEmail);
         //click edit button for third account
         WebElement accountEditButton = DriverManager.getWebDriver().findElement(By.xpath( "/html[1]/body[1]/div[1]/div[3]/div[1]/form[1]/table[1]/tbody[1]/tr[3]/td[5]/input[2]"));
         accountEditButton.click();
 
         //insert new email
-        WebElement emailField = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/div[1]/form[1]/table[1]/tbody[1]/tr[4]/td[2]/input[1]"));
+        WebElement emailField = DriverManager.getWebDriver().findElement(By.name("EditForm:email"));
         WaitForElement.waitUntilElementIsVisible(emailField);
         emailField.sendKeys("testowy@email.com");
-        System.out.println(emailField);
 
 
         //click save button
-        WebElement saveButton = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/div[1]/form[1]/input[2]"));
-        WaitForElement.waitUntilElementIsVisible(saveButton);
+        WebElement saveButton = DriverManager.getWebDriver().findElement(By.name("EditForm:j_idt32"));
         saveButton.click();
 
-        //click dropdown once again
-        WebElement dropdownAccountsAgain = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/nav[1]/div[1]/div[2]/ul[1]/li[4]/a[1]"));
-        WaitForElement.waitUntilElementIsVisible(dropdownAccountsAgain);
-        dropdownAccountsAgain.click();
-
-        //click list accounts once again
-        WebElement accountButtonAgain = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/nav[1]/div[1]/div[2]/ul[1]/li[4]/ul[1]/li[2]/a[1]"));
-        accountButtonAgain.click();
-
-        //get current email of third account
-        String currentEmailAfterChanges = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/div[1]/form[1]/table[1]/tbody[1]/tr[3]/td[4]")).getText();
-
-        assertNotEquals(currentEmailAfterChanges, currentEmail);
-        assertEquals(currentEmailAfterChanges, "testowy@email.com");
-
-        //click edit button for third account once again
-        WebElement accountEditButtonAgain = DriverManager.getWebDriver().findElement(By.xpath( "/html[1]/body[1]/div[1]/div[3]/div[1]/form[1]/table[1]/tbody[1]/tr[3]/td[5]/input[2]"));
-        accountEditButtonAgain.click();
-
-        //insert new email
-        WebElement emailFieldAgain = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/div[1]/form[1]/table[1]/tbody[1]/tr[4]/td[2]/input[1]"));
-        WaitForElement.waitUntilElementIsVisible(emailFieldAgain);
-        emailField.sendKeys(currentEmail);
-
-        //click save button
-        WebElement saveButtonAgain = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/div[1]/form[1]/input[2]"));
-        WaitForElement.waitUntilElementIsVisible(saveButtonAgain);
-        saveButtonAgain.click();
+//        //click dropdown once again
+//        WebElement dropdownAccountsAgain = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/nav[1]/div[1]/div[2]/ul[1]/li[4]/a[1]"));
+//        WaitForElement.waitUntilElementIsVisible(dropdownAccountsAgain);
+//        dropdownAccountsAgain.click();
+//
+//        //click list accounts once again
+//        WebElement accountButtonAgain = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/nav[1]/div[1]/div[2]/ul[1]/li[4]/ul[1]/li[2]/a[1]"));
+//        accountButtonAgain.click();
+//
+//        //get current email of third account
+//        String currentEmailAfterChanges = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/div[1]/form[1]/table[1]/tbody[1]/tr[3]/td[4]")).getText();
+//
+//        assertNotEquals(currentEmailAfterChanges, currentEmail);
+//        assertEquals(currentEmailAfterChanges, "testowy@email.com");
+//
+//        //click edit button for third account once again
+//        WebElement accountEditButtonAgain = DriverManager.getWebDriver().findElement(By.xpath( "/html[1]/body[1]/div[1]/div[3]/div[1]/form[1]/table[1]/tbody[1]/tr[3]/td[5]/input[2]"));
+//        accountEditButtonAgain.click();
+//
+//        //insert new email
+//        WebElement emailFieldAgain = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/div[1]/form[1]/table[1]/tbody[1]/tr[4]/td[2]/input[1]"));
+//        WaitForElement.waitUntilElementIsVisible(emailFieldAgain);
+//        emailField.sendKeys(currentEmail);
+//
+//        //click save button
+//        WebElement saveButtonAgain = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/div[1]/form[1]/input[2]"));
+//        WaitForElement.waitUntilElementIsVisible(saveButtonAgain);
+//        saveButtonAgain.click();
     }
 }
