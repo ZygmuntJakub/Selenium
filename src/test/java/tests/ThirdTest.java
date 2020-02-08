db0c90d4070d12d080ba6f296ce563de1e1a7b00
Shumitu <darkdragon5255@gmail.com>
2020-02-08 13:29:26 +0100

    next test

--- src/test/java/tests/ThirdTest.java
+++ src/test/java/tests/ThirdTest.java
@@ -48,6 +48,8 @@ public class ThirdTest extends TestBase {
         WebElement emailField = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/div[1]/form[1]/table[1]/tbody[1]/tr[4]/td[2]/input[1]"));
         WaitForElement.waitUntilElementIsVisible(emailField);
         emailField.sendKeys("testowy@email.com");
+        System.out.println(emailField);
+
 
         //click save button
         WebElement saveButton = DriverManager.getWebDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/div[1]/form[1]/input[2]"));
